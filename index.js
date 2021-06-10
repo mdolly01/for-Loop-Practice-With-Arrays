68676f1dd45dcb0eb898defffb5157f10e7b0aab
MattDolly <61992537+mdolly01@users.noreply.github.com>
2021-06-10 17:47:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// create an array variable containing the names
+dogs = ["Chena", "Abby", "Kiya", "Chula", "Sitka", "Ohana"]
+
+// write a for loop that prints each name on a different line
+for (let i = 0; i < dogs.length; i++) {
+  console.log(dogs[i])
+}
